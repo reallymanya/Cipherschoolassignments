64bdaa6f8c44fa9d0ec65e5f68401441642365cd
reallymanya <manyatakkar.01@gmail.com>
2024-08-25 14:54:59 +0530

    DAY 04

--- DAY 04/Q2.cpp
+++ DAY 04/Q2.cpp
@@ -1,9 +1,37 @@
-/*Create a Complex class to represent complex numbers and overload the +, -, operators to perform arithmetic operations on complex numbers.
+#include<iostream>
+using namespace std;
+class Complex{
+    private:
+    int real; 
+    int image;
+    public: 
+    Complex(int r=0, int i=0):
+    real(r), image(i){ }
+    Complex operator + (const Complex& other){
+        return Complex(real + other.real, image + other.image);
+    }
+    Complex operator - (const Complex& other){
+        return Complex(real - other.real, image - other.image);
+    }
+    void display() const{
+        cout<<real<<" + "<<image<<"i"<<endl;
+    }
+};
 
-Sample Output:-
+int main(){
+    Complex c1(3,4);
+    Complex c2(1, 2);
+    cout<<"Complex Number 1: ";
+    c1.display();
+    cout<<"Complex Number 2: ";
+    c2.display();
+    Complex sum = c1+c2;
+    Complex difference = c1-c2;
+    cout<<"Sum: ";
+    sum.display();
+    cout<<"Difference: ";
+    difference.display();
+    return 0;
+}
 
-Complex Number 1: 3 + 4i
-Complex Number 2: 1 + 2i
-Sum: 4 + 6i
-Difference: 2 + 2i*/
 
